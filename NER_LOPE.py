9f150575c134a582193f04639f5a5576695aab13
Miguel Betti <miguelbetti@hotmail.com>
2024-05-30 09:47:17 +0200

    train

---
+++ NER_LOPE.py
@@ -0,0 +1,45 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+from flair.data import Sentence 
+from flair.models import SequenceTagger
+tagger = SequenceTagger.load("ner_bertSpanish_fineTuning2/final-model.pt")
+import re
+import os
+
+clean_unicode = {
+    "B-ORG": "O",
+    "B-MISC": "O",
+    "B-PER": "O",
+    "I-ORG": "O",
+    "I-MISC": "O",
+    "I-PER": "O"  
+}
+
+
+folder = 'corpus_full_clean'
+
+for f in os.listdir(folder):
+    if f.endswith('.txt'):
+        txt_path = os.path.join(folder, f)
+        label, ext = os.path.splitext(f)
+        with open(txt_path, encoding='utf-8') as t:
+            text = t.read()
+            text_normalized = " ".join(text.split())
+            sentence = Sentence(text_normalized)
+            tagger.predict(sentence)
+            for entity in sentence.get_spans('ner'):
+                prefix = 'B-'
+                for token in entity:
+                    token.set_label('ner-bio', prefix + entity.tag)
+                    prefix = 'I-'
+            output = ""        
+            for token in sentence:
+                output += str(token.get_label('ner-bio'))
+                output += "\n"
+            result = re.sub(r"([A-Z]\w+\[[0-9]+\]:\s\")([A-Z a-z 0-9 .,?!:¿¡…\'\*´ `’‘℣Đ=æǝ°>\—\-\_–\(\)\¬\«\»;~óòáàâíìñéèúüùäëïöÀÁÉÈÊËÍÌÎÏÑÓÒÔÖÚÙÛÜ\"\"]+)(\"\s→\s)([A-Z-]*)(\s\([0-9 .]*\))", r"\2 \4", output)
+            for c in clean_unicode:
+                result = re.sub(c, clean_unicode[c], result)
+            with open ('corpus_out/' + label + '.txt', 'w', encoding='utf-8') as x:
+                x.write(result)
+
---
+++ NER_LOPE_TRAIN.py
@@ -0,0 +1,49 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+
+from flair.data import Corpus
+from flair.datasets import ColumnCorpus
+from flair.embeddings import TransformerWordEmbeddings
+from flair.models import SequenceTagger
+from flair.trainers import ModelTrainer
+import torch, gc
+
+columns = {0: 'text', 1: 'ner'}
+data_folder = 'corpus_train'
+corpus: Corpus = ColumnCorpus(data_folder, columns, train_file='train.txt', test_file='test.txt', dev_file='dev.txt')
+label_type = 'ner'
+label_dict = corpus.make_label_dictionary(label_type=label_type, add_unk=False)
+
+embeddings = TransformerWordEmbeddings(
+    model='mrm8488/bert-spanish-cased-finetuned-ner',
+    layers='-1',
+    subtoken_pooling='first',
+    fine_tune=True,
+    use_context=True,
+    model_max_length=512
+)
+
+tagger = SequenceTagger(
+    hidden_size=256,
+    embeddings=embeddings,
+    tag_dictionary=label_dict,
+    tag_type='ner',
+    use_crf=False,
+    use_rnn=False,
+    reproject_embeddings=False
+)
+
+trainer = ModelTrainer(tagger, corpus)
+
+gc.collect()
+torch.cuda.empty_cache()
+
+trainer.fine_tune(
+    'ner_bertSpanish_fineTuning',
+    learning_rate=5.0e-6,
+    mini_batch_size=4,
+    mini_batch_chunk_size=1,
+    max_epochs=20,
+    embeddings_storage_mode='gpu'
+)
